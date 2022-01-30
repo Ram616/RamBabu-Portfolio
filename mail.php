446fab02ad570c1863af42f7c4d1030943d7567d
Ram616 <59142683+Ram616@users.noreply.github.com>
2022-01-30 16:19:04 +0530

    u

--- mail.php
+++ mail.php
@@ -2,11 +2,12 @@
 //get data from form  
 $name = $_POST['name'];
 $email= $_POST['email'];
+$project= $_POST['project'];
 $message= $_POST['message'];
-$to = "youremail@mail.com";
+$to = "ramswami0021@mail.com";
 $subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n  Project = " . $project . "\r\n Message =" . $message;
+$headers = "From: noreply@ramswami.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
